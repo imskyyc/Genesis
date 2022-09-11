f392dbbed212eccf67837126eb2c4287ebb7b929
imskyyc <santafejayyt@gmail.com>
2022-09-10 20:01:53 -0700

    first commit

---
+++ src/server/README.server.lua
@@ -0,0 +1,9 @@
+--[[
+
+    this is just something I'm working on for fun, as I really don't have a framework I'm proud of
+    whether or not this is implemented is entirely dependent on if it's needed
+    :)
+
+    -sky
+
+]]--
